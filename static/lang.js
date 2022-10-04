ce3b765e84bda1d3c076f9a011aa65436a5a6659
josephanimate2021 <jackiecrosman@gmail.com>
2022-10-03 23:00:10 -0400

    stuff

--- static/lang.js
+++ static/lang.js
@@ -4,10 +4,12 @@ const fs = require("fs");
 
 module.exports = function (req, res) {
   if (req.method != "GET") return;
-  const match = req.url.match(/\/static\/client_theme\/go\/lang_common\/([^/]+)$/);
+  const match = req.url.match(/\/static\/client_theme\/([^/]+)\/([^/]+)\/([^/]+)$/);
   if (!match) return;
-  const id = match[1];
-  get(env.CLIENT_URL + `/client_theme/go/lang_common/${id}`).then(b => res.end(b)).catch(e => {
+  const type = match[1];
+  const stuff = match[2];
+  const id = match[3];
+  get(`https://josephanimate2021.github.io/static/477/client_theme/${type}/${stuff}/${id}`).then(b => res.end(b)).catch(e => {
     console.log(e); 
     res.end('404 Not Found'); 
   });
