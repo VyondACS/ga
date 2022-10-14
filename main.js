5e7deaa212629cff992d5be1ea4cfed66aa6a952
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-10-14 09:25:55 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
-if (!fs.existsSync("./files")) fs.mkdirSync("./files");
 
 const env = {
   hostname: '127.0.0.1',
@@ -18,7 +17,6 @@ const env = {
   node_env: "dev"
 };
 const folder = env.ASSETS_FOLDER, sFolder = `${folder}/sounds`;
-if (!fs.existsSync(folder)) fs.mkdirSync(folder);
 if (!fs.existsSync(sFolder)) fs.mkdirSync(sFolder);
 env.BG_FOLDER = `${folder}/backgrounds`;
 env.PROPS_FOLDER = `${folder}/props`;
@@ -43,14 +41,12 @@ const utilities = [
 ];
 
 if (!fs.existsSync(env.MOVIE_FOLDER)) fs.mkdirSync(env.MOVIE_FOLDER);
-if (!fs.existsSync(env.STARTER_FOLDER)) fs.mkdirSync(env.STARTER_FOLDER);
 if (!fs.existsSync(env.BG_FOLDER)) fs.mkdirSync(env.BG_FOLDER);
 if (!fs.existsSync(env.PROPS_FOLDER)) fs.mkdirSync(env.PROPS_FOLDER);
 if (!fs.existsSync(env.SOUNDS_FOLDER)) fs.mkdirSync(env.SOUNDS_FOLDER);
 if (!fs.existsSync(env.MUSIC_FOLDER)) fs.mkdirSync(env.MUSIC_FOLDER);
 if (!fs.existsSync(env.VOICEOVERS_FOLDER)) fs.mkdirSync(env.VOICEOVERS_FOLDER);
 if (!fs.existsSync(env.CHARS_FOLDER)) fs.mkdirSync(env.CHARS_FOLDER);
-if (!fs.existsSync(env.DATABASES_FOLDER)) fs.mkdirSync(env.DATABASES_FOLDER);
 Object.assign(process.env, require("./env"));
 const server = http.createServer((req, res) => {
   try {
